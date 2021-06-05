91fc5cf4fba604d48bac8dc7359cceab10cc59f8
Ava Marie Bullock <ava.m.bullock@gmail.com>
2021-06-05 13:53:06 -0600

    began About component, verified that it displays when app is started

---
+++ src/components/About/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function About() {
+    return(
+        <section>
+            <h1 id="about">Testing. One, a-two, a-three! *crunch*</h1>
+        </section>
+    )
+}
+
+export default About;
