f060bba22eebea1af188e024a1b0ef05bbe17c4f
Ava Marie Bullock <ava.m.bullock@gmail.com>
2021-06-05 14:34:02 -0600

    added basic stylings, created color palette and imported bulma

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,9 +1,25 @@
 import React from 'react';
 
 function About() {
-    return(
+    return (
         <section>
-            <h1 id="about">Testing. One, a-two, a-three! *crunch*</h1>
+            <div className="columns is-mobile">
+                <div className="column is-half is-offset-one-quarter">
+                    <h1 className="title" id="about">
+                        Testing. One, a-two, a-three! *crunch*
+                        </h1>
+                </div>
+            </div>
+            <div className="columns is-mobile">
+                <div className="column is-three-fifths is-offset-one-fifth">
+                    <p>
+                        Hello, and welcome to my portfolio! My name is Ava Bullock, and I'm a Full Stack Web Developer.
+                            <br></br>When I'm not coding up a storm, I enjoy playing story-driven video games, playing with my cats, and cooking!
+                            <br></br>If you'd like to see the projects I've worked on, please select the "projects" tab above!
+                            I look forward to working with you. ♥
+                        </p>
+                </div>
+            </div>
         </section>
     )
 }
