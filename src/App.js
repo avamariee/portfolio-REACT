234767a6e35d56de87d98509fc9741990c1ab899
Ava Marie Bullock <ava.m.bullock@gmail.com>
2021-06-12 19:15:59 -0600

    set up skeleton for project component, forgot to switch branches

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './assets/styles.css';
 
 import About from './components/About';
 import Nav from './components/Nav';
+import Projects from './components/Projects'
 
 function App() {
   return (
---
+++ src/components/Projects/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Projects(){
+    return(
+        <section>
+            <h1>Testing</h1>
+        </section>
+    )
+}
+
+export default Projects;
