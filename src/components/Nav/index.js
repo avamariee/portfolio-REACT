aef711440804e255f99813ec6fbe545840c981e7
Ava Marie Bullock <ava.m.bullock@gmail.com>
2021-06-12 19:04:31 -0600

    added an array to hopefully help render each component on my navbar

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -2,17 +2,33 @@ import React from 'react';
 
 function Nav() {
 
-    
+    const navItems = [
+        {
+            name: 'Portfolio',
+            description: 'A list of all of the projects I have contributed to.'
+        },
+        {
+            name: 'Contact',
+            description: 'Contact information to reach me.'
+        },
+        {
+            name: 'Resume',
+            description: 'A link to download my resume.'
+        }
+    ]
+
+
     return (
         <nav className="navbar" role="navigation">
             
             <div className="navbar-brand">
                 <h1 className="nav-title grad">Ava Bullock</h1>
             </div>
+            
 
             <div className="navbar-menu grad">
                 <div className="navbar-start">
-                    <a className="navbar-item grad" href="#about">
+                    <a className="navbar-item grad" href="/">
                         About Me
                   </a>
                     <a className="navbar-item grad" href="#portfolio">
