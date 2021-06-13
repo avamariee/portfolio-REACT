9f33445a4c7600633cafb114cfddcd6c86448971
Ava Marie Bullock <ava.m.bullock@gmail.com>
2021-06-13 12:57:11 -0600

    realized i needed 6 projects, so added another one to the portfolio page

--- src/components/ProjectList/index.js
+++ src/components/ProjectList/index.js
@@ -5,66 +5,76 @@ import JustMyType from '../../assets/images/just-my-type.JPG'
 import WeatherDashboard from '../../assets/images/weather-dashboard.jpg'
 import YourFavTech from '../../assets/images/your-fav-tech-blog.JPG'
 import WorkDayScheduler from '../../assets/images/work-day-scheduler.jpg'
+import PizzaHunt from '../../assets/images/pizza-hunt.JPG'
 
 function ProjectList() {
 
-  
+
     return (
         <div className="images is-fullwidth">
-          <div className="columns">
-               <div className="column is-half is-offset-one-quarter">
-                   <h2 className="port-title is-centered">Pet Adoption Quiz</h2>
-                   <a className="image is-3by1" href="https://avamariee.github.io/pet-adoption-quiz/" target="_blank" rel="noreferrer">
-                    <img
-                        src={DogsVsCats}
-                        alt="A quiz website to see if you are a dog or a cat person, complete with facts about pet adoptions, and adoption links."
-                    />
-                    </a>
-               </div>
-          </div>
             <div className="columns">
+                <div className="column is-half ">
+                    <h2 className="port-title is-centered">Pet Adoption Quiz</h2>
+                    <a className="image is-3by1" href="https://avamariee.github.io/pet-adoption-quiz/" target="_blank" rel="noreferrer">
+                        <img
+                            src={DogsVsCats}
+                            alt="A quiz website to see if you are a dog or a cat person, complete with facts about pet adoptions, and adoption links."
+                        />
+                    </a>
+                </div>
                 <div className="column is-half">
                     <h2> Just My Type </h2>
-                <a className="image is-3by1" href="https://stark-depths-18071.herokuapp.com/" target="_blank" rel="noreferrer">
-                    <img
-                        src={JustMyType}
-                        alt="Example"
-                    />
+                    <a className="image is-3by1" href="https://stark-depths-18071.herokuapp.com/" target="_blank" rel="noreferrer">
+                        <img
+                            src={JustMyType}
+                            alt="A typing game website where the user can test their typing skills."
+                        />
+                    </a>
+                </div>
+            </div>
+            <div className="columns">
+                <div className="column is-half">
+                    <h2> Pizza Hunt </h2>
+                    <a className="image is-3by1" href="https://frozen-savannah-71044.herokuapp.com/" target="_blank" rel="noreferrer">
+                        <img
+                            src={PizzaHunt}
+                            alt="A website where the user can create pizza recipes, and comment on other users' recipes."
+                        />
                     </a>
                 </div>
                 <div className="column is-half">
                     <h2> Weather Dashboard </h2>
-                <a className="image is-3by1"href="https://avamariee.github.io/weather-dashboard/" target="_blank" rel="noreferrer">
-                    <img
-                        src={WeatherDashboard}
-                        alt="Example"
-                    />
+                    <a className="image is-3by1" href="https://avamariee.github.io/weather-dashboard/" target="_blank" rel="noreferrer">
+                        <img
+                            src={WeatherDashboard}
+                            alt="A website where you can search for weather information in any city."
+                        />
                     </a>
                 </div>
             </div>
-            
-                <div className="columns">
-                    <div className="column is-half">
-                        <h2> Your Favorite Tech Blog </h2>
+
+            <div className="columns">
+                <div className="column is-half">
+                    <h2> Your Favorite Tech Blog </h2>
                     <a className="image is-3by1" href="https://pacific-escarpment-48422.herokuapp.com/" target="_blank" rel="noreferrer">
                         <img
                             src={YourFavTech}
-                            alt="Example"
+                            alt="A fictional tech blog."
                         />
-                        </a>
-                    </div>
-                     <div className="column is-half">
-                         <h2> Work Day Scheduler </h2>
-                     <a className="image is-3by1" href="https://avamariee.github.io/work-day-scheduler/" target="_blank" rel="noreferrer">
-                         <img
-                            src={WorkDayScheduler}
-                            alt="Example"
-                        />
-                        </a>
-                     </div>
+                    </a>
                 </div>
-            
-            
+                <div className="column is-half">
+                    <h2> Work Day Scheduler </h2>
+                    <a className="image is-3by1" href="https://avamariee.github.io/work-day-scheduler/" target="_blank" rel="noreferrer">
+                        <img
+                            src={WorkDayScheduler}
+                            alt="An application used to schedule a standard 8 hour work day."
+                        />
+                    </a>
+                </div>
+            </div>
+
+
         </div>
     )
 }
